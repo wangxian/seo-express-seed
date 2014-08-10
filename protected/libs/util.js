60816a03b6f17347cd67f3bf16fd61b098934562
wangxian <xian366@126.com>
2014-08-10 16:31:01 +0800

    CHANGED: 增加搜索引擎

--- protected/libs/util.js
+++ protected/libs/util.js
@@ -1,7 +1,7 @@
 
 // 是否是搜索引擎
-var spiderRegexp = new RegExp('Baiduspider|Googlebot|BingBot|curl', 'i')
+var spiderRegexp = new RegExp("Baiduspider|Googlebot|BingBot|MSNBot|YoudaoBot|Sosospider|360Spider|Sogou web spider|Sogou inst spider|curl|Wget", "i");
 exports.isRobots = function (req) {
-  var ua = req.get("user-agent")
+  var ua = req.get("user-agent");
   return spiderRegexp.test(ua);
-}
+};
